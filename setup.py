b91ff7b9a06658578de300bf3bf8d7490de3c9f9
ponyliuxb <91177038+ponyliuxb@users.noreply.github.com>
2023-02-17 19:59:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-with open("README.md") as f:
+with open("README.md", 'r', encoding='utf-8') as f:
     readme = f.read()
 
-with open("requirements.txt") as f:
+with open("requirements.txt", 'r', encoding='utf-8') as f:
     requirements = f.read().split()
 
 setup(name="homura",
